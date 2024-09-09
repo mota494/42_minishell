e107570b35a4e9174081984e0544ad98668b2631
Sofia Bueno <130552467+sofiabuenos@users.noreply.github.com>
2024-09-09 23:50:03 +0100

    struct prototypes

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: sofiabueno <sofiabueno@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 15:35:04 by mloureir          #+#    #+#             */
-/*   Updated: 2024/09/09 17:23:37 by sofiabueno       ###   ########.fr       */
+/*   Updated: 2024/09/09 21:50:13 by sofiabueno       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,30 @@
 # include <sys/wait.h>
 # include <errno.h>
 # include "includes/libft/libft.h"
+
+
+/*structs*/
+
+typedef enum s_types
+{
+	string,
+	pipe,
+	command,
+	builtin,
+}	t_types;
+
+/*token list*/
+typedef struct s_token
+{
+	char			*cmd_line;
+	t_types			type;
+	struct s_token	*next;
+}			t_token;
+
+/*main structure - work in progress*/
+typedef struct s_shell
+{
+	t_token	*token;
+}			t_shell;
 
 #endif
