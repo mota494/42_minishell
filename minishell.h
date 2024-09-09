4aa708cdd262e732628126a1ca8d73a8dac55912
Sofia Bueno <130552467+sofiabuenos@users.noreply.github.com>
2024-09-09 17:59:57 +0100

    check_input and read_command

--- minishell.h
+++ minishell.h
@@ -3,24 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mloureir <mloureir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sofiabueno <sofiabueno@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 15:35:04 by mloureir          #+#    #+#             */
-/*   Updated: 2024/09/09 15:35:05 by mloureir         ###   ########.fr       */
+/*   Updated: 2024/09/09 17:23:37 by sofiabueno       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
+# include <stdio.h>
 # include <readline/history.h>
 # include <readline/readline.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <errno.h>
-#include "includes/libft/libft.h"
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <errno.h>
+# include "includes/libft/libft.h"
 
 #endif
