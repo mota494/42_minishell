9ec41b2696c58bcd821a8b2196f7acadcc56c966
Mota494 <migueloureiromota@gmail.com>
2024-09-10 16:44:05 +0100

    boa sorte Sofia, esta a separar os comandos direito

---
+++ src/utils.c
@@ -0,0 +1,67 @@
+#include "../minishell.h"
+
+int	ft_strcmp(char *tocomp, char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (tocomp[i] == str[i] && tocomp[i] && str[i])
+		i++;
+	if (i == strlen(tocomp) && i == strlen(str))
+		return (1);
+	return (0);
+}
+
+char	*alocpy(char *str)
+{
+	char	*toret;
+	int		i;
+
+	toret = malloc(ft_strlen(str) * sizeof(char));
+	i = 0;
+	while (str[i])
+	{
+		toret[i] = str[i];
+		i++;
+	}
+	toret[i] = '\0';
+	return (toret);
+}
+
+t_token	*add_node(char	*content)
+{
+	t_token	*new_node;
+
+	new_node = malloc(sizeof(t_token));
+	new_node->cmd_line = alocpy(content);
+	new_node->next = NULL;
+	new_node->type = get_type(new_node->cmd_line);
+	return (new_node);
+}
+
+int	ft_ispace(int c)
+{
+	if ((c >= 9 && c <= 13) || c == 32)
+		return (1);
+	return (0);
+}
+
+int	is_builtin(char *str)
+{
+	if (ft_strcmp("echo", str))
+		return (1);
+	else if (ft_strcmp("cd", str))
+		return (1);
+	else if (ft_strcmp("pwd", str))
+		return (1);
+	else if (ft_strcmp("export", str))
+		return (1);
+	else if (ft_strcmp("unset", str))
+		return (1);
+	else if (ft_strcmp("env", str))
+		return (1);
+	else if (ft_strcmp("exit", str))
+		return (1);
+	else
+		return (0);
+}
