591c2c6240a5ccf7729d343fdbb4a8f744724849
Mota494 <migueloureiromota@gmail.com>
2024-09-11 16:55:41 +0100

    only spaces left

---
+++ src/utils2.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mloureir <mloureir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/11 16:30:34 by mloureir          #+#    #+#             */
+/*   Updated: 2024/09/11 16:50:01 by mloureir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	jump_spaces(char *line)
+{
+	while (*line == 32)
+		line++;
+}
+
+int	ft_ispace(int c)
+{
+	if ((c >= 9 && c <= 13) || c == 32)
+		return (1);
+	return (0);
+}
+
+int	ft_strcmp(char *tocomp, char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (tocomp[i] == str[i] && tocomp[i] && str[i])
+		i++;
+	if (i == ft_strlen(tocomp) && i == ft_strlen(str))
+		return (1);
+	return (0);
+}
+
+char	*strjoinchr(char *str, char c)
+{
+	char	*newstr;
+	size_t	i;
+
+	newstr = malloc(ft_strlen(str) + sizeof(char) + 1);
+	i = 0;
+	while (i < ft_strlen(str))
+	{
+		newstr[i] = str[i];
+		i++;
+	}
+	newstr[i] = c;
+	newstr[i + 1] = '\0';
+	free(str);
+	return (newstr);
+}
