1aae8f84324dd92c46dda5c29786d75efa4bc7f4
Mota494 <migueloureiromota@gmail.com>
2024-11-05 15:06:16 +0000

    execution

--- src/500_execution.c
+++ src/500_execution.c
@@ -1,41 +1,279 @@
-/* ************************************************************************** */
-/*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   500_execution.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mloureir <mloureir@42porto.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/04 09:37:03 by mloureir          #+#    #+#             */
-/*   Updated: 2024/11/04 10:39:11 by mloureir         ###   ########.fr       */
+/*   Created: 2024/10/18 13:57:50 by mloureir          #+#    #+#             */
+/*   Updated: 2024/10/18 14:37:03 by mloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+#include <stdio.h>
+#include <unistd.h>
 
-void	exec_builtin(t_shell *cmd)
+/**printf para debug - remover
+ * malloc here!
+ */
+int	create_pipes(t_shell *cmd)
 {
-	if (sstrcmp(cmd->token->cmd_line, "cd") == 1)
-		cd(cmd);
-	else if (sstrcmp(cmd->token->cmd_line, "exit") == 1)
-		cmd->error_code = exit_main(cmd);
-	else if (sstrcmp(cmd->token->cmd_line, "echo") == 1)
-		echo_main(cmd);
-	else if (sstrcmp(cmd->token->cmd_line, "pwd") == 1)
-		pwd(cmd);
-	else if (sstrcmp(cmd->token->cmd_line, "env") == 1)
-		builtin_env();
-}
+	int i = 0;
 
-int	execute_pipeline(t_shell *cmd, char **envp)
-{
-	int	i;
-
-	i = 0;
-	if (cmd->n_builtin == 1 && cmd->n_inputs == 1)
-		exec_builtin(cmd);
-	while (i < cmd->n_inputs)
+	cmd->p_fds = (t_fd *)malloc(sizeof(t_fd) * (cmd->n_inputs - 1));
+	if (!cmd->p_fds)
 	{
+		perror("Memory allocation issue - pipe fds");
+		return (1);
+	}
+	while (i < cmd->n_inputs - 1)
+	{
+		if (pipe(cmd->p_fds[i].fd) == -1)
+		{
+			perror("Error creating pipes");
+			return (2);
+		}
+		i++;
+	}
+	return (0);
+}
+/* aloca memória para os Process ids retornados 
+pela chamada da função fork*/
+int	alloc_pids(t_shell *cmd)
+{
+	cmd->pids = (pid_t *)malloc(sizeof(pid_t) * cmd->n_inputs);
+	if (!cmd->pids)
+	{
+		perror("Memory allocation issue - alloc_pids");
+		return (1);
 	}
 	return (0);
 }
 
+/* conta quantos comandos e builtins há na pipeline */
+int	count_tokens(t_token *current_token)
+{
+	int count;
+	
+	count = 0;
+	while (current_token && sstrcmp(current_token->cmd_line, "|") != 1)
+	{
+		count++;
+		current_token = current_token->next;
+	}
+	return (count);
+}
+/** Função auxiliar que identifica os comandos/builtins e seus argumentos
+ * Retorna  matriz de strings comando e argumentos
+ * segundo argumento da execve. */
+char	**get_command_tokens(t_token *token)
+{
+	int		num_token;
+	char	**args;
+	t_token	*current;
+	int		i;
+
+	current = token;
+	num_token = count_tokens(current);
+	args = malloc(sizeof(char *) * (num_token + 1));
+	if (!args)
+		return (NULL);
+	i = 0;
+	while (current && strcmp(current->cmd_line, "|") != 0)
+	{
+		args[i] = alocpy(current->cmd_line);
+		current = current->next;
+		i++;
+	}
+	args[i] = NULL;
+	return (args);
+}
+
+/*libera a matriz de argumentos dos comandos e builtins*/
+void	free_args(char **args)
+{
+	int i;
+
+	i = 0;
+	while (args[i])
+	{
+		free(args[i]);
+		i++;
+	}
+	free(args);
+}
+
+/* redireciona a entrada e saída dos cmds/builtins para
+ * os processos se comunicarem via pipes */
+void	handle_redirection(t_shell *cmd, int i)
+{
+	if (i == 0)
+		first_redirect(cmd);
+	else if (i == cmd->n_inputs - 1)
+		last_redirect(cmd, i);
+	else
+		std_redirect(cmd, i);
+}
+
+/* Verifica se o comando é um built-in e o executa se for.
+ * Retorna 1 se for built-in, 0 se não for. */
+int	call_builtin(char **args, t_shell *cmd)
+{
+	if (!args || !args[0])
+		return (0);
+	if (sstrcmp(args[0], "cd") == 1)
+	{
+		cd(cmd);
+		return (1);
+	}
+	else if (sstrcmp(args[0], "exit") == 1)
+	{
+		cmd->error_code = exit_main(cmd);
+		return (1);
+	}
+	else if (sstrcmp(args[0], "echo") == 1)
+	{
+		echo_main(cmd);
+		return (1);
+	}
+	else if (sstrcmp(args[0], "pwd") == 1)
+		return (pwd(cmd));
+	else if (sstrcmp(args[0], "env") == 1)
+	{
+		builtin_env();
+		return (1);
+	}
+	return (0);
+}
+
+/** No caso da execução corresponder a apenas um builtin,
+ * a funçao one_builtin executa-o no processo pai */
+int	one_builtin(t_shell *cmd)
+{
+	t_token	*current_token;
+	char	**args;
+
+	current_token = cmd->token;
+	if (current_token->type == builtin)
+	{
+		args = get_command_tokens(current_token);
+		while (current_token)
+		{
+			if (current_token->type == pipes)
+				return (1);
+			current_token = current_token->next;
+		}
+		call_builtin(args, cmd);
+		free_args(args);
+		return (0);
+	}
+	return (1);
+}
+
+/*executa os builtins*/
+int	execute_builtin(t_shell *cmd, t_token *token)
+{
+	t_token	*current_token;
+	char	**args;
+
+	current_token = token;
+	args = get_command_tokens(current_token);
+	close_fds(cmd);
+	call_builtin(args, cmd);
+	free_args(args);
+	return (0);
+}
+
+/*executa os comandos*/
+int	execute_command(t_shell *cmd, t_token *token, char **envp)
+{
+	t_token	*current_token;
+	char	**args;
+
+	current_token = token;
+	args = get_command_tokens(current_token);
+	close_fds(cmd);
+	write(1, "comando será executado\n", 10);
+	write(1, &current_token->path_name, ft_strlen(current_token->path_name));
+	if (execve(current_token->path_name, args, envp) == -1)
+	{
+		perror("execve");
+		return (1);
+	}
+	return (0);
+}
+
+void	run_cmdx_builtx(t_shell *cmd, t_token *current, char **envp)
+{
+	if (current && current->type == builtin)
+		execute_builtin(cmd, current);
+	else if (current && current->type == command)
+		execute_command(cmd, current, envp);
+	exit(0);
+}
+
+t_token	*get_next(t_token *current)
+{
+	t_token *temp;
+
+	temp = current->next;
+	while (temp)
+	{
+		if (temp->type == builtin || temp->type == command)
+			break ;
+		temp = temp->next;
+	}
+	return (temp);
+}
+
+int	execute_pipeline(t_shell *cmd, char **envp)
+{
+	int		i;
+	int	p[2];
+	int	help;
+
+	if (cmd->n_inputs == 1 && cmd->n_builtin == 1)
+	{
+		one_builtin(cmd);
+		return (0);
+	}
+	if ((create_pipes(cmd) != 0) || (alloc_pids(cmd) != 0))
+		return (1);
+	i = 0;
+	help = dup(STDIN_FILENO);
+	while (cmd->token)
+	{
+		pipe(p);
+		//handle_redirection(cmd, i);
+		cmd->pids[i] = fork();
+		if (cmd->pids[i] == -1)
+		{
+			perror("fork issue");
+			return (1);
+		}
+			if (cmd->pids[i] == 0)
+			{
+					if (i != 0)
+			{
+				dup2(help, STDIN_FILENO);
+			}
+			else
+			{
+				dup2(p[1], STDOUT_FILENO);
+			}
+			close(p[0]);
+			close(p[1]);
+			close(help);
+			//close_fds(cmd);
+			run_cmdx_builtx(cmd, cmd->token, envp);
+		}
+		dup2(p[0], help);
+		close(p[0]);
+		close(p[1]);
+		i++;
+		cmd->token = get_next(cmd->token);
+	}
+	close(help);
+	wait_for_child(cmd);
+	close_fds(cmd);
+	return (0);
+}
--- src/501_redirect_and_wait.c
+++ src/501_redirect_and_wait.c
@@ -3,12 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   501_redirect_and_wait.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mloureir <mloureir@42porto.com>            +#+  +:+       +#+        */
+/*   By: sofiabueno <sofiabueno@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/04 09:37:06 by mloureir          #+#    #+#             */
-/*   Updated: 2024/11/04 09:49:07 by mloureir         ###   ########.fr       */
+/*   Created: 2024/09/30 17:11:19 by sofiabueno        #+#    #+#             */
+/*   Updated: 2024/11/05 14:52:15 by mloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+/** Verificar redirects (< > >>)
+antes ou depois do comando dentro dessas funcoes? */
+
+/** writes output on the writing end of pipe instead of on stdout
+ ** still reads input from stdin */
+void	first_redirect(t_shell *cmd)
+{
+	dup2(cmd->p_fds[0].fd[1], STDOUT_FILENO);
+	close(cmd->p_fds[0].fd[0]);
+}
+
+/** reads input from the last pipe readind end 
+ ** still returns output in stdout */
+void	last_redirect(t_shell *cmd, int i)
+{
+	dup2(cmd->p_fds[i - 1].fd[0], STDIN_FILENO);
+	close(cmd->p_fds[i - 1].fd[0]);
+}
+
+/** reads input from the last pipe readind end 
+ ** writes output on the writing end of pipe instead of on stdout*/
+void	std_redirect(t_shell *cmd, int i)
+{
+	dup2(cmd->p_fds[i - 1].fd[0], STDIN_FILENO);
+	dup2(cmd->p_fds[i].fd[1], STDOUT_FILENO);
+	close(cmd->p_fds[i - 1].fd[0]);
+	close(cmd->p_fds[i].fd[1]);
+}
+
+void	close_fds(t_shell *cmd)
+{
+	int	i;
+
+	i = -1;
+	while (++i < cmd->n_inputs - 1)
+	{
+		close(cmd->p_fds[i].fd[0]);
+		close(cmd->p_fds[i].fd[1]);
+	}
+}
+
+int	wait_for_child(t_shell *cmd)
+{
+	int	i;
+	int	wstatus;
+
+	i = -1;
+	while (++i < cmd->n_inputs)
+	{
+		waitpid(cmd->pids[i], &wstatus, 0);
+	}
+	// if (WIFEXITED(wstatus))
+	// 	return (WEXITSTATUS(wstatus));
+	//return (-1);
+	return (0);
+}
