068564d0ba09c8308a4fe532e38a7a358382217d
Mota494 <migueloureiromota@gmail.com>
2024-11-12 15:14:01 +0000

    var parsing almost there

--- src/004_utils5.c
+++ src/004_utils5.c
@@ -6,11 +6,18 @@
 /*   By: mloureir <mloureir@42porto.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 09:39:22 by mloureir          #+#    #+#             */
-/*   Updated: 2024/10/31 15:01:23 by mloureir         ###   ########.fr       */
+/*   Updated: 2024/11/12 14:38:12 by mloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+int	is_quote(int c)
+{
+	if (c == 34 || c == 39)
+		return (1);
+	return (0);
+}
 
 int	find_quote_type(char *str)
 {
