2af5817ca3754a83297aa1e468c1eaa8e394fa01
Mota494 <migueloureiromota@gmail.com>
2024-12-16 09:47:03 +0000

    solved some memory leaks

--- src/000_utils.c
+++ src/000_utils.c
@@ -6,7 +6,7 @@
 /*   By: sofiabueno <sofiabueno@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 16:19:40 by mloureir          #+#    #+#             */
-/*   Updated: 2024/12/12 17:28:48 by mloureir         ###   ########.fr       */
+/*   Updated: 2024/12/16 09:46:00 by mloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,12 +35,16 @@ char	*alocpy(char *str)
 t_token	*add_node(t_shell *cmd, char *content, int id)
 {
 	t_token	*new_node;
+	char	*helper;
 
 	new_node = malloc(sizeof(t_token));
 	new_node->orig_line = alocpy(content);
 	free(content);
 	new_node->cmd_line = parser_vars(new_node);
-	new_node->cmd_line = parse_quotes(new_node->cmd_line);
+	helper = alocpy(new_node->cmd_line);
+	free(new_node->cmd_line);
+	new_node->cmd_line = parse_quotes(helper);
+	free(helper);
 	new_node->next = NULL;
 	new_node->type = get_type(cmd, new_node, new_node->cmd_line,
 			new_node->orig_line);
--- src/101_parser_get_replace_var.c
+++ src/101_parser_get_replace_var.c
@@ -6,34 +6,18 @@
 /*   By: mloureir <mloureir@42porto.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 14:54:42 by mloureir          #+#    #+#             */
-/*   Updated: 2024/12/12 14:19:52 by mloureir         ###   ########.fr       */
+/*   Updated: 2024/12/16 09:41:54 by mloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	free_replaced(char *oldtoret, char *var_name)
+char	*return_var_value(char *oldtoret, char *var_value)
 {
-	free(oldtoret);
-	free(var_name);
-}
-
-char	*get_replace_var(char *orig_line, int *pos, char *oldtoret)
-{
-	char	*var;
-	char	*var_value;
 	int		i;
 	char	*newtoret;
 
-	var = initalize_str();
-	*pos += 1;
 	newtoret = alocpy(oldtoret);
-	while (orig_line[*pos] && (orig_line[*pos] == '_' || ft_isalpha(orig_line[*pos])))
-	{
-		var = strjoinchr(var, orig_line[*pos]);
-		*pos += 1;
-	}
-	var_value = get_env(var);
 	if (var_value)
 	{
 		i = 0;
@@ -43,6 +27,25 @@ char	*get_replace_var(char *orig_line, int *pos, char *oldtoret)
 			i++;
 		}
 	}
-	free_replaced(oldtoret, var);
+	free(oldtoret);
+	return (newtoret);
+}
+
+char	*get_replace_var(char *orig_line, int *pos, char *oldtoret)
+{
+	char	*var;
+	char	*newtoret;
+	char	*var_value;
+
+	var = initalize_str();
+	*pos += 1;
+	while (orig_line[*pos] && (orig_line[*pos] == '_' || ft_isalpha(orig_line[*pos])))
+	{
+		var = strjoinchr(var, orig_line[*pos]);
+		*pos += 1;
+	}
+	var_value = get_env(var);
+	newtoret = return_var_value(oldtoret, var_value);
+	free(var);
 	return (newtoret);
 }
